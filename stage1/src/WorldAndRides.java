121ef87d091913626fdb009b07488f9a5e85f52e
Joel Kimani <jk626@kent.ac.uk>
2022-01-27 21:20:02 +0000

    Version 1.2 1.Added a cars class to store the data of each vehicle independently 2.Made some changes to the world and ride class to read and store the data of   the file

--- stage1/src/WorldAndRides.java
+++ stage1/src/WorldAndRides.java
@@ -6,7 +6,18 @@
  * @version (a version number or a date)
  */
 public class WorldAndRides
-
+    int[] worldData;
+    int [] rideData;
+    int worldRow;
+    int worldColumns;
+    int vehicles;
+    int rides;
+    int bonus;
+    int steps;
+    int[] location;
+    int[] destination;
+    int start;
+    int finish;
     //TODO decide a proper data-structure to store the information about the world
     //and the requested rides.
 
@@ -18,6 +29,50 @@ public class WorldAndRides
         //TODO read the information about the requested rides and store the
         //information in this class
     }
-    
+
+    public void readFile(String worldAndRidesFileName)
+    {
+        int count = 1;
+        try {
+            Scanner myReader = new Scanner(worldAndRidesFileName);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (count == 1)
+                {
+                    worldData = data.split("\\s");
+                    setWorld(worldData);
+                }
+                else
+                {
+                    rideData = data.split("\\s");
+                    setRide(rideData);
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
     //TODO define appropriate methods for this class.
+
+    public void setWorld(int [] worldData)
+        {
+            worldRow = worldData[0];
+            WorldColumn = worldData[1];
+            vehicle = worldData[2];
+            rides = worldData[3];
+            bonus = worldData[4];
+            steps = worldData[5];
+        }
+
+    public void setRide (int [] rideData)
+        {
+            location[0]= rideData[0];
+            location[1]= rideData[1];
+            destination[0]= rideData[3];
+            destination[1]= rideData[4];
+            start = rideData[5];
+            finish = rideData[6];
+        }
 }
